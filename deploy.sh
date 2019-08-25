2daacc44460debf29acf96fae6b8f48b89c2ca0f
Jane Lawson <janel4wson@gmail.com>
2019-08-25 14:52:41 +0100

    added git sha on the tags

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-docker build -t jlaws/multi-client -f ./client/Dockerfile ./client
-docker build -t jlaws/multi-server -f ./server/Dockerfile ./server
-docker build -t jlaws/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t jlaws/multi-client:latest -t jlaws/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jlaws/multi-server:latest -t jlaws/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jlaws/multi-worker:latest -t jlaws/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-kubectl rollout restart deployment/server-deployment
-kubectl rollout restart deployment/client-deployment
-kubectl rollout restart deployment/worker-deployment
+docker push jlaws/multi-client:latest 
+docker push jlaws/multi-server:latest 
+docker push jlaws/multi-worker:latest 
 
-docker push jlaws/multi-client
-docker push jlaws/multi-server
-docker push jlaws/multi-worker
+docker push jlaws/multi-client:$SHA 
+docker push jlaws/multi-server:$SHA 
+docker push jlaws/multi-worker:$SHA
 
 kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=jlaws/multi-client:$SHA
+kubectl set image deployments/server-deployment server=jlaws/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=jlaws/multi-worker:$SHA
