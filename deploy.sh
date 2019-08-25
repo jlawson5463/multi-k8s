d8f76123f7f9be77e76a17007fa1f3d7072cb008
Jane Lawson <janel4wson@gmail.com>
2019-08-25 08:50:29 +0100

    trying kubectl restart script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 docker build -t jlaws/multi-client -f ./client/Dockerfile ./client
 docker build -t jlaws/multi-server -f ./server/Dockerfile ./server
 docker build -t jlaws/multi-worker -f ./worker/Dockerfile ./worker
+
+kubectl rollout restart deployment/server-deployment
+kubectl rollout restart deployment/client-deployment
+kubectl rollout restart deployment/worker-deployment
+
 docker push jlaws/multi-client
 docker push jlaws/multi-server
 docker push jlaws/multi-worker
+
 kubectl apply -f k8s
